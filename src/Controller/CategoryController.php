c88b8b8cb2c332f7647bd818030b6542b9014b79
echchadli140 <echchadlihajar@email.com>
2026-05-03 01:27:22 +0100

    Etape 2 : dynamisation des pages category et product

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Controller;
 
+use App\Repository\CategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -8,14 +9,22 @@ use Symfony\Component\Routing\Attribute\Route;
 final class CategoryController extends AbstractController
 {
     #[Route('/browse-categories', name: 'app_browse_categories')]
-    public function index(): Response
+    public function index(CategoryRepository $categoryRepository): Response
     {
-        return $this->render('browse_categories.html.twig');
+        $categories = $categoryRepository->findAll();
+
+        return $this->render('browse_categories.html.twig', [
+            'categories' => $categories,
+        ]);
     }
 
-    #[Route('/products-by-category', name: 'app_products_by_category')]
-    public function productsByCategory(): Response
+    #[Route('/products-by-category/{id}', name: 'app_products_by_category')]
+    public function productsByCategory(int $id, CategoryRepository $categoryRepository): Response
     {
-        return $this->render('products_by_category.html.twig');
+        $category = $categoryRepository->find($id);
+
+        return $this->render('products_by_category.html.twig', [
+            'category' => $category,
+        ]);
     }
 }
